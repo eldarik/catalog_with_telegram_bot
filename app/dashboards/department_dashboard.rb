a03cc94daf4eccad5181b9b723cded5f46245f0c
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2017-11-11 00:02:59 +0300

    update administrate views

--- app/dashboards/department_dashboard.rb
+++ app/dashboards/department_dashboard.rb
@@ -8,6 +8,10 @@ class DepartmentDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    name: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -16,20 +20,26 @@ class DepartmentDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-
+    :id,
+    :name,
+    :created_at,
+    :updated_at,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-
+    :id,
+    :name,
+    :created_at,
+    :updated_at,
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-
+    :name,
   ].freeze
 
   # Overwrite this method to customize how departments are displayed
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :clients
     resources :departments
+    resources :categories
+    resources :products
 
     root to: "clients#index"
   end
