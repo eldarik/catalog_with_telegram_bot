4fed549509ce020e59accbe68060bab41ad29517
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2017-11-11 15:48:59 +0300

    add product page renderers

---
+++ lib/telegram_shop_bot/page_renderers/detailed_product.rb
@@ -0,0 +1,29 @@
+class TelegramShopBot::PageRenderers::Product < TelegramShopBot::PageRenderers::Base
+  attr_accessor :product
+  def initialize(args)
+    @product = args[:product]
+    args[:image_paths] = get_image_paths
+    args[:text_messages] |= generate_text_messages
+    args[:keyboard_buttons] |= generate_keyboard_buttons
+    super
+  end
+
+  private
+  def get_image_paths
+    #todo add images to products
+    []
+  end
+
+  def generate_keyboard_buttons
+    [
+      { text: 'добавить к заказу', callback_data: "order/product/#{id}" }
+    ]
+  end
+
+  def generate_text_messages
+    [
+      product.name,
+      product.description
+    ]
+  end
+end
