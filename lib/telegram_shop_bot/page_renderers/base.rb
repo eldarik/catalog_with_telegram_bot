9e7a0f11f685777ac4c602c1de7a798d1774c0b0
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2017-11-11 14:29:00 +0300

    add main chat page

--- lib/telegram_shop_bot/page_renderers/base.rb
+++ lib/telegram_shop_bot/page_renderers/base.rb
@@ -1,45 +1,45 @@
 class TelegramShopBot::PageRenderers::Base
   attr_accessor :text_messages, :keyboard, :images
-  attr_reader :bot, :receipent_id
+  attr_reader :bot, :recipient_id
 
   def initialize(args)
     @bot = args[:bot]
     raise 'bot is required' unless bot.present?
-    @receipent_id = args[:receipent_id]
-    raise 'receipent_id is required' unless receipent_id.present?
+    @recipient_id = args[:recipient_id]
+    raise 'recipient_id is required' unless recipient_id.present?
 
     initialize_keyboard(args[:keyboard_buttons])
     initialize_images(args[:image_paths])
     @text_messages = args[:text_messages]
   end
 
-  def render_for_receipent
-    send_images
-    send_text_messages
-    send_keyboard
+  def render_for_recipient
+    render_images
+    render_text_messages
+    render_keyboard
   end
 
   private
   def render_images
     images&.each do |image|
-      bot.api.send_photo(chat_id: receipent_id, photo: image)
+      bot.api.send_photo(chat_id: recipient_id, photo: image)
     end
   end
 
   def render_text_messages
     text_messages&.each do |m|
-      bot.api.send_message(chat_id: receipent_id, text: m)
+      bot.api.send_message(chat_id: recipient_id, text: m)
     end
   end
 
   def render_keyboard
-    bot.api.send_message(chat_id: receipent_id, text: '_', reply_markup: keyboard)
+    bot.api.send_message(chat_id: recipient_id, text: 'Выбирете дальнейшее действие', reply_markup: keyboard)
   end
 
   def initialize_keyboard(keyboard_buttons)
     buttons =
       keyboard_buttons.map do |b|
-        Telegram::Bot::Types::InlineKeyboardButton.new(b.slice(:text, :callback_data)
+        Telegram::Bot::Types::InlineKeyboardButton.new(b.slice(:text, :callback_data))
       end
     @keyboard =
       markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: buttons)
--- lib/telegram_shop_bot/page_renderers/start.rb
+++ lib/telegram_shop_bot/page_renderers/start.rb
@@ -1,13 +1,6 @@
 class TelegramShopBot::PageRenderers::Start < TelegramShopBot::PageRenderers::Base
 
-  def initialize(args)
-    @bot = args[:bot]
-    raise 'bot is required' unless bot.present?
-    @receipent_id = args[:receipent_id]
-    raise 'receipent_id is required' unless receipent_id.present?
-  end
-
-  def render_for_receipent
+  def render_for_recipient
     render_request_of_phone_number
   end
 
@@ -16,7 +9,7 @@ class TelegramShopBot::PageRenderers::Start < TelegramShopBot::PageRenderers::Ba
     keyboard = Telegram::Bot::Types::KeyboardButton.new(text: 'разрешить', request_contact: true),
     markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
     bot.api.send_message(
-      chat_id: receipent.id, text: 'доступ к номеру телефона', reply_markup: markup
+      chat_id: recipient.id, text: 'доступ к номеру телефона', reply_markup: markup
     )
   end
 end
