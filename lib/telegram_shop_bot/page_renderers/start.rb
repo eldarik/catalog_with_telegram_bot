8a1004a5605d1135857f6f96addbfd4fd45f9609
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2017-11-11 14:08:19 +0300

    add start bot page

--- lib/telegram_shop_bot/page_renderers/start.rb
+++ lib/telegram_shop_bot/page_renderers/start.rb
@@ -5,7 +5,18 @@ class TelegramShopBot::PageRenderers::Start < TelegramShopBot::PageRenderers::Ba
     raise 'bot is required' unless bot.present?
     @receipent_id = args[:receipent_id]
     raise 'receipent_id is required' unless receipent_id.present?
+  end
 
-    initialize_keyboard(args[:keyboard_buttons])
+  def render_for_receipent
+    render_request_of_phone_number
+  end
+
+  private
+  def render_request_of_phone_number
+    keyboard = Telegram::Bot::Types::KeyboardButton.new(text: 'разрешить', request_contact: true),
+    markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
+    bot.api.send_message(
+      chat_id: receipent.id, text: 'доступ к номеру телефона', reply_markup: markup
+    )
   end
 end
