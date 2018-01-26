33b9d26abeadbf19cc8bd7707a2d6119efc5fad7
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2018-01-26 21:43:51 +0300

    update current order page render

--- lib/telegram_shop_bot/page_renderers/current_order.rb
+++ lib/telegram_shop_bot/page_renderers/current_order.rb
@@ -1,27 +1,20 @@
 class TelegramShopBot::PageRenderers::CurrentOrder < TelegramShopBot::PageRenderers::Base
   attr_accessor :order
+
   def initialize(args)
     @order = args[:order]
-    order.each do |product|
-      render_product(args.merge(product))
+    if order.present?
+      args[:text_messages] = [
+        ::Product.find(args['product_id']).name,
+        "количество #{args['count']}"
+      ]
+      args[:keyboard_buttons] = [
+        { text: 'удалить', callback_data: "products/#{args['product_id']}/remove_from_order" }
+      ]
+    else
+      args[:text_messages] = ['ваша корзина пуста']
     end
+
     super
-  end
-
-  private
-
-  def render_product(args)
-    TelegramShopBot::PageRenderers::Base.new(
-      args.merge(
-        text_messages: [
-          ::Product.find(args['product_id']).name,
-          "количество #{args['count']}"
-        ],
-        keyboard_buttons: [
-          { text: 'удалить', callback_data: "products/#{args['product_id']}/remove_from_order" },
-          #todo add update count
-        ]
-      )
-    ).render_for_recipient
   end
 end
