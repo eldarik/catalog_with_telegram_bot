d7f0b39209f919ec5870e5457d4b85c165e4d71a
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2018-01-31 17:39:28 +0300

    add process remove order

--- app/services/process_message.rb
+++ app/services/process_message.rb
@@ -76,6 +76,8 @@ class ProcessMessage < Service
         process_add_to_order(product_id: $1)
       elsif message[:data] =~ /products\/(\d+)\/remove_from_order/
         process_remove_from_order(product_id: $1)
+      elsif message[:data] =~ /orders\/(\d+)\/remove/
+        process_remove_order(order_id: $1)
       end
     end
   end
@@ -265,6 +267,17 @@ class ProcessMessage < Service
           text_messages: ['нет сохраненных заказов']
         ).render_for_recipient
       end
+    end
+  end
+
+  def process_remove_order(order_id:)
+    order = Client.find_by(telegram_uid: message[:user_id])&.orders&.find(order_id)
+    order.destroy
+    @bot.run do |bot|
+      TelegramShopBot::PageRenderers::Base.new(
+        bot: bot, recipient_id: message[:user_id],
+        text_messages: ['заказ отменен']
+      ).render_for_recipient
     end
   end
 end
