a34021eabb46848bef539500a52bffeb00e46e42
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2018-02-04 18:29:24 +0300

    update locales

--- app/dashboards/category_dashboard.rb
+++ app/dashboards/category_dashboard.rb
@@ -8,9 +8,9 @@ class CategoryDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    department: Field::BelongsTo,
     id: Field::Number,
     name: Field::String,
+    department: Field::BelongsTo,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -19,17 +19,17 @@ class CategoryDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :department,
     :id,
     :name,
+    :department,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :department,
     :id,
     :name,
+    :department,
   ].freeze
 
   # FORM_ATTRIBUTES
