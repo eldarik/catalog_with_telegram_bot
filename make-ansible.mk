32790df239f037d3218c94174be2725fec020bb4
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-03-23 10:11:08 +0700

    add production ansible vars

--- make-ansible.mk
+++ make-ansible.mk
@@ -1,7 +1,6 @@
 ansible-development-update-env:
 	docker run  -v $(CURDIR):/usr/src/app -w /app williamyeh/ansible:ubuntu18.04 ansible-playbook ansible/development.yml -i ansible/development -vv --tag env
 
-
 ansible-development-setup-env:
 	ansible-playbook ansible/development.yml -i ansible/development -vv
 
@@ -10,3 +9,12 @@ ansible-development-vaults-encrypt:
 
 ansible-development-vaults-decrypt:
 	ansible-vault decrypt ansible/development/group_vars/all/vault.yml
+
+ansible-vaults-encrypt:
+	ansible-vault encrypt ansible/production/group_vars/all/vault.yml
+
+ansible-vaults-decrypt:
+	ansible-vault decrypt ansible/production/group_vars/all/vault.yml
+
+ansible-vaults-edit:
+	ansible-vault edit ansible/production/group_vars/all/vault.yml
