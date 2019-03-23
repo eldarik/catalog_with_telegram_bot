21047c799ee63b4c40466822824fefdc76814af0
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-03-23 16:34:09 +0700

    add deploy playbook

--- make-production.mk
+++ make-production.mk
@@ -2,3 +2,12 @@ U := deploy
 
 production-setup:
 	ansible-playbook ansible/site.yml -i ansible/production --ask-become-pass
+
+production-env-update:
+	ansible-playbook ansible/site.yml -i ansible/production -u $U --tag env --ask-become-pass
+
+production-deploy:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U
+
+production-deploy-app:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --tag app
